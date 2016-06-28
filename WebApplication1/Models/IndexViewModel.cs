cea472938887257c7177601e307b8570baadb637
NarawitK <tarantia.sg@gmail.com>
2016-06-28 17:31:06 +0700

    EOD01
    - Added Pagination

---
+++ WebApplication1/Models/IndexViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace WebApplication1.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<String> items{ get; set; }
+        public int pager { get; set; }
+    }
+    public class Pager
+    {
+        public int Totalitems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+
+        public Pager(int totalItems, int? page, int pageSize = 10)
+        {
+            var totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            var currentPage = page != null ? (int)page : 1;
+            var startPage = currentPage + 5;
+            var endPage = currentPage - 4;
+            if (startPage <= 0)
+            {
+                endPage -= (startPage - 1);
+                startPage = 1;
+            }
+            if (endPage > totalPages)
+            {
+                endPage = totalPages;
+                if (endPage > 10)
+                {
+                    startPage = endPage - 9;
+                }
+            }
+            Totalitems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+        }
+    }
+}
