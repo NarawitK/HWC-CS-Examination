6856139095f6fc2e00b69acc34f943d90fbcee73
NarawitK <tarantia.sg@gmail.com>
2016-07-11 11:21:25 +0700

    Font Included

--- WebApplication1/Controllers/HR_EmployeeController.cs
+++ WebApplication1/Controllers/HR_EmployeeController.cs
@@ -66,7 +66,7 @@ namespace WebApplication1.Controllers
                 }
                 catch
                 {
-                    return View();
+                    return RedirectToAction("Index");
                 }
                 
             };
